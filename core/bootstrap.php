a6e7fdaa08d683968da95b098c0794841d50f3f5
danieldolz <danieldolz@iesebre.com>
2016-09-27 18:11:25 +0200

    commit inicial

---
+++ core/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'functions.php';
+
+require 'Models/Task.php';
+
+require 'database/Connection.php';
+
+require 'database/QueryBuilder.php';
+
+$config = require 'config/database.php';
+
+$message = require 'config/message.php';
+
+$pdo = Connection::make($config,$message);
+
+$query = new QueryBuilder($pdo);
